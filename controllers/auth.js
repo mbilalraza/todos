66257a00c602b158a1f0d484993bc05ae64985c5
mbilalraza <m.bilalraza@yahoo.com>
2020-07-30 10:04:44 +0500

    User register api end-point.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,77 @@
+const Joi = require('joi');
+const HttpStatus = require('http-status-codes');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+const Helpers = require('../Helpers');
+const config = require('../config/development');
+
 module.exports = {
   async CreateUser(req, res) {
+    const schema = Joi.object().keys({
+      username: Joi.string()
+        .min(5)
+        .max(10)
+        .required(),
+      email: Joi.string()
+        .email()
+        .required(),
+      password: Joi.string()
+        .min(5)
+        .required()
+    });
+
+    const { error, value } = Joi.validate(req.body, schema);
+    if (error && error.details) {
+      return res.status(HttpStatus.BAD_REQUEST).json({ msg: error.details })
+    }
+
+    const userEmail = await User.findOne({
+      email: Helpers.lowerCase(req.body.email)
+    });
+    if (userEmail) {
+      return res
+        .status(HttpStatus.CONFLICT)
+        .json({ message: 'Email already exist' });
+    }
+
+    const userName = await User.findOne({
+      username: Helpers.firstUpper(req.body.username)
+    });
+    if (userName) {
+      return res
+        .status(HttpStatus.CONFLICT)
+        .json({ message: 'Username already exist' });
+    }
+
+    return bcrypt.hash(value.password, 10, (err, hash) => {
+      if (err) {
+        return res
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ message: 'Error hashing password' });
+      }
+      const body = {
+        username: Helpers.firstUpper(value.username),
+        email: Helpers.lowerCase(value.email),
+        password: hash
+      };
+      User.create(body)
+        .then(user => {
+          const token = jwt.sign({ data: user }, config.secret, {
+            expiresIn: '5h'
+          });
+          res.cookie('auth', token);
+          res
+            .status(HttpStatus.CREATED)
+            .json({ message: 'User created successfully', user, token });
+        })
+        .catch(err => {
+          res
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json({ message: 'Error occured' });
+        });
+    });
   },
   async LoginUser(req, res) {
   }
